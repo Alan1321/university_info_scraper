28836e84406d771af586828f34c8793f4355d60c
Alan123 <alansubedi123@gmail.com>
2024-01-09 00:12:00 -0600

    editing constnts.py

--- scrape_credit_hours/constants.py
+++ scrape_credit_hours/constants.py
@@ -1,5 +1,6 @@
-# TERM="fall2023"
-TERM="sprg2024"
+TERM="sprg2024" #replace as needed --> fall2023, spring2024, fall2020 etc
+credit_hour = '1.0' #replace as needed --> 1.0, 2.0, 3.0
+
 BASE_URL = f"https://www.uah.edu/cgi-bin/schedule.pl?file={TERM}.html&segment=NDX"
 course_abbreviations = ["ACC", "AES", "AHS", "ARH", "ARS", "AST", "BLS", "BSE", "BUS", "BYS",
                         "CE", "CFC", "CH", "CHE", "CM", "CPE", "CS", "EA", "ECH", "ECN", "ED",
@@ -8,5 +9,3 @@ course_abbreviations = ["ACC", "AES", "AHS", "ARH", "ARS", "AST", "BLS", "BSE", 
                         "MKT", "MSC", "MTS", "MU", "MUA", "MUE", "MUJ", "MUX", "NUA", "NUR", "NUS",
                         "OCS", "OPE", "OPT", "OSE", "PH", "PHL", "PRO", "PSC", "PY", "SFM", "SOC",
                         "SPA", "SSC", "TH", "WGS", "WLC"]
-
-credit_hour = '1.0'
